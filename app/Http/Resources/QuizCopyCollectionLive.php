8bfd8c29d3707f8878fd96d381715c024da8f250
longl <longlehoang2013@gmail.com>
2021-08-15 17:57:04 +0700

    add created_at attribute

--- app/Http/Resources/QuizCopyCollectionLive.php
+++ app/Http/Resources/QuizCopyCollectionLive.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class QuizCopyCollectionLive extends JsonResource
@@ -17,6 +18,7 @@ class QuizCopyCollectionLive extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'created_at' => Carbon::parse($this->created_at)->toDayDateTimeString(),
             'number_question' => count($this->questionCopies),
             'questions' => QuestionCopyCollectionShort::collection($this->questionCopies),
         ];
